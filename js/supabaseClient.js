56cb70f103703f47da4acb5d93e10608e037e17b
iemcdo <casadeoracioncm@gmail.com>
2026-02-03 16:23:00 -0600

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // js/supabaseClient.js
-const SUPABASE_URL = "https://zmvazbrtpqbuvrghinq.supabase.co";
+const SUPABASE_URL = "https://zmvazbrtpqbvurghginq.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptdmF6YnJ0cHFidnVyZ2hnaW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMzk1MTMsImV4cCI6MjA4NTcxNTUxM30.KAuHA-Du37lsixyXzEWVrh-M_-BYeK6Hhb-DqlH2BYs";
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
